72e943a9bda85734ae4dae47b0d20e6d063ecf5b
alfayedtch <albos7484@gmail.com>
2021-03-08 08:57:05 +0100

    Creation des variables

---
+++ variables.dart
@@ -0,0 +1,16 @@
+main(){
+  //Pour declarer une variable nous pouvons utiliser var si on ne connait pas le type ou si le type sera variable tout au long du projet.
+  var name = "al-fayed";
+  print(name);
+  // !!attention nous ne pouvons pas modifier cette variable une fois instancier en un autre type  ex:name = 12;
+
+  int age = 18;
+
+  print("je m'appelle $name et j'ai $age");
+  //ou
+  print("je m'appelle "+name+" et j'ai $age");
+
+  double pi = 3.14;
+
+  print(pi);
+}
