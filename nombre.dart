7b65052f1bd6130dc7c25b2123319738c25d0c8f
alfayedtch <albos7484@gmail.com>
2021-03-08 23:53:35 +0100

    les nombres

---
+++ nombre.dart
@@ -0,0 +1,21 @@
+main(){
+  int entier = 5;
+   
+  int addition = entier + entier;
+  print(addition);
+   
+  int soustraction = entier - entier;
+
+  print(soustraction);
+
+  int multiplication = (entier * entier);
+
+  print (multiplication);
+
+  double pi = 3.146587687879;
+
+  print(pi.toStringAsFixed(3));
+  print(pi.ceil());
+  print(pi.floor());
+
+}
