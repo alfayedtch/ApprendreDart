04e7f54febf81817f749334ab1d9f7b3c852bcde
alfayedtch <albos7484@gmail.com>
2021-03-08 23:00:34 +0100

    constantes et final

---
+++ constFinal.dart
@@ -0,0 +1,17 @@
+main(){
+    const age = 18;
+    const nom = "al-fayed";
+
+    print(age);
+    print(nom);
+
+    final nombre = 12;
+    
+    final force = nombre*2;
+
+    print(force);
+
+    final masse = age *3;
+
+    print(masse);
+}
